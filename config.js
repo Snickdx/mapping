9585801ff9b5a5eb0bb2451733facef237be867b
Nicholas Mendez <snickdx@gmail.com>
2021-05-12 15:03:01 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbFrhY3c-2AB05oyjYFRupQd4RguxaqCE",
+  authDomain: "course-mapper-6c734.firebaseapp.com",
+  projectId: "course-mapper-6c734",
+  storageBucket: "course-mapper-6c734.appspot.com",
+  messagingSenderId: "313309940632",
+  appId: "1:313309940632:web:d1a79d42da5309a7b979ed"
+};
