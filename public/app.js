a4656fe37ce2b45711c45bff5dd02d8f0c990cec
Nicholas Mendez <snickdx@gmail.com>
2021-05-15 20:41:20 +0000

    ui updates, fixed saving, counts and color ui

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 import {cstopics, csdomains, cssubdomains} from './csmodule.js';
 import { firebaseConfig } from './config.js';
-import { getUser, getCourseTopics, saveCourseTopics, validateEmail } from './users.js';
+import { getUser, getCourseTopics, saveCourseTopics } from './users.js';
 
 
 firebase.initializeApp(firebaseConfig);
@@ -8,8 +8,6 @@ let db = firebase.firestore();
 
 let changes= false;
 let selectedCourse=null;
-let courses=nulll;
-
 const topics = [];
 
 //########################### Window Functions #########################
@@ -24,7 +22,7 @@ window.courseSelected = function(event){
         loadForm(value);
         showPending(false);
       }else{
-        event.target.value = selectedCourse
+        event.target.value = selectedCourse;
       }
     }else{
       showPending(false);
@@ -34,12 +32,21 @@ window.courseSelected = function(event){
 
 }
 
-window.updateCount = function(){
+window.updateCount = function(event){
   const form = document.querySelector('#myform');
   const selectedTopics = new FormData(form).getAll('topics');
   const numTopics = selectedTopics.length;
+  const topic = event.target.value;
+  let array = topic.split('-');
+  let domain = array[0].trim();
+  let subdomain = `${domain}-${array[1].trim()}`;
+  
+  updateStatsUI(domain, event.target.checked);
+  updateStatsUI(subdomain, event.target.checked);
+
+  
   showPending(true)
-  document.querySelector('#count').innerHTML = numTopics;
+  showCount(numTopics);
 }
 
 
@@ -49,8 +56,10 @@ window.selectTopics = async function(event){
   const selected = formData.getAll('topics');
   const course = formData.get('course');
   let auth = firebase.auth().currentUser;
+  showLoader();
   await saveCourseTopics(auth.email, course, selected, db);
   showPending(false);
+  hideLoader();
   M.toast({html: `${selected.length} Topics saved to ${course} !`, classes: 'rounded'});
 }
 
@@ -62,8 +71,49 @@ window.logout = async function(){
 
 // ########################## Module Functions #########################
 
+function getStats(topics){
+  const result = {};
+  for(let topic of topics){
+    let array = topic.split('-');
+    let domain = array[0].trim();
+    let subdomain = `${domain}-${array[1].trim()}`;
+    
+    if(domain in result){
+      result[domain]++;
+    }else{
+      result[domain]=1;
+    }
+
+    if(subdomain in result){
+      result[subdomain]++;
+    }else{
+      result[subdomain]=1;
+    }
+  }
+  return result;
+}
+
+function updateStatsUI(target, increment){
+  const elem = document.querySelector(`#${target}`); 
+  let num = elem.innerHTML.trim();
+  num = parseInt(num);
+
+  if(increment)
+    num++;
+  else
+    num--;
+  
+  elem.innerHTML = num;
+  
+}
+
+function getColor(num){
+  let color = [ 'red lighten-5', 'pink lighten-5', 'purple lighten-5', 'deep-purple lighten-5', 'indigo lighten-5', 'cyan lighten-5', 'teal lighten-5', 'green lighten-5', 'lime lighten-5', 'yellow lighten-5', 'orange lighten-5', 'deep-orange lighten-5' ];
+  return color[num%color.length];
+}
+
 function showCount(num){
-  document.querySelector('#count').innerHTML = num;
+  document.querySelector('#count').innerHTML = `<div class="chip">${num} Topics in Total</div>`;
 }
 
 function showPending(pending){
@@ -71,6 +121,8 @@ function showPending(pending){
   const html = pending ? `<div class="chip red white-text">Pending Changes<i class="close material-icons">warning</i></div>` : '<div class="chip green white-text">All Changes Saved<i class="close material-icons">check</i></div>';
   document.querySelector('#pending').innerHTML = html;          
 }
+
+
 
 function showHelpModal(){
 }
@@ -84,7 +136,7 @@ function topicsTemplate(subdomain, selected){
     html+=`
       <p>
         <label>
-          <input type="checkbox" onclick="updateCount()" ${ selected.includes(topicVal) ? 'checked' : '' } name="topics" value="${topicVal}" />
+          <input type="checkbox" onclick="updateCount(event)" ${ selected.includes(topicVal) ? 'checked' : '' } name="topics" value="${topicVal}" />
           <span class="black-text">${topicVal}</span>
         </label>
       </p>
@@ -95,15 +147,18 @@ function topicsTemplate(subdomain, selected){
 
 }
 
-function subDomainTemplate(domain, selectedTopics){
+function subDomainTemplate(domain, selectedTopics, stats){
   let html='';
 
   for(let subdomain in domain){
     html+=`
         <li>
-          <div class="collapsible-header">${subdomain} -  ${cssubdomains[subdomain]}</div>
-            <div class="collapsible-body">
-              ${topicsTemplate(domain[subdomain], selectedTopics)}                    
+          <div class="collapsible-header">
+            <div class="col s11">${subdomain} -  ${cssubdomains[subdomain]}</div>
+            <div class="col s1"><div class="chip" id="${subdomain}"> ${stats[subdomain] ?  stats[subdomain]:'0' }</div></div>
+          </div>
+            <div class="collapsible-body white">
+              ${topicsTemplate(domain[subdomain], selectedTopics, stats)}                    
             </div>
         </li>
     `;
@@ -111,57 +166,76 @@ function subDomainTemplate(domain, selectedTopics){
   return html;
 }
 
+function showLoader(){
+  const loader = document.querySelector('#loader');
+  loader.style.display = 'block';
+}
+
+function hideLoader(){
+  const loader = document.querySelector('#loader');
+  loader.style.display = 'none'
+}
+
 async function loadForm(course){
   let html="";
 
-  const loader = document.querySelector('#loader');
-  loader.innerHTML = '<div class="progress"><div class="indeterminate"></div></div>';
+  showLoader();
 
   selectedCourse = course;
 
   let auth = firebase.auth().currentUser;
 
   const selectedTopics = await getCourseTopics(auth.email, course, db);
+  let domainCount = 0;
+  let stats = getStats(selectedTopics);
 
   for(let domain in cstopics){
 
       html+=`
       <li>
-        <div class="collapsible-header">${domain} - ${csdomains[domain]}</div>
-          <div class="collapsible-body">
+        <div class="collapsible-header ${getColor(domainCount)}">
+        <div class="col s11">${domain} - ${csdomains[domain]}</div>
+        <div class="col s1"><div class="chip" id="${domain}"> ${stats[domain] ?  stats[domain]:'0' }</div></div>
+      
+        </div>
+          <div class="collapsible-body ${getColor(domainCount)}">
             <ul class="collapsible">
-              ${subDomainTemplate(cstopics[domain], selectedTopics)}
+              ${subDomainTemplate(cstopics[domain], selectedTopics, stats)}
             </ul>
           </div>
       </li>
       `;
+      domainCount++;
   }
 
-  loader.innerHTML = '';
+  hideLoader();
 
-  showCount(selectedTopics.length)
+  showCount(selectedTopics.length);
 
   document.querySelector('#input').innerHTML = html;
   
   M.Collapsible.init(document.querySelectorAll('.collapsible'));
 }
 
-function displayUser(auth){
+async function displayUser(auth){
 
-  if(!validateEmail(auth.email)){
-    alert(`No user found for ${auth.email}`);
-  }else{
+  let {courses, name} = await getUser(auth.email, db);
 
-    let {courses, name} = getUser(auth.email);
-    let html=' <option disabled value="default" selected>Select Course</option>';
-    for(let course in courses){
-      html+=`<option>${course}</option>`;
-    }
-    const courseSelect = document.querySelector('#course');
-    courseSelect.innerHTML = html;
-    M.FormSelect.init(courseSelect);
-    document.querySelector('#name').innerHTML = 'Welcome '+name+' V0.4';
+  if(name !== 'Unkown'){
+    document.querySelector('#fab').style.display = 'block';
   }
+
+  let html=' <option disabled value="default" selected>Select Course</option>';
+  
+  for(let course in courses){
+    html+=`<option>${course}</option>`;
+  }
+  const courseSelect = document.querySelector('#course');
+  courseSelect.innerHTML = html;
+  M.FormSelect.init(courseSelect);
+  document.querySelector('#name').innerHTML = 'Welcome '+name+' V0.5';
+
+  hideLoader();
  
 }
 
--- public/users.js
+++ public/users.js
@@ -1,54 +1,18 @@
-const users = {
-  "snickdx@gmail.com":{
-    courses:{
-      "INFO 1601":[],
-      "INFO 3601":[],
-      "INFO 2602":[]
-    },
-    name:"Nicholas Mendez"
-  },
-  "kris.manohar@sta.uwi.edu":{
-    courses:{
-      "COMP 1600":[]
-    },
-    name:"Kris Manohar"
-  },
-  "shareeda.mohammed@sta.uwi.edu":{
-    courses:{
-      "COMP 1601":[]
-    },
-    name:"Shareeda Mohammed"
-  },
-  "phaedra.mohammed@sta.uwi.edu":{
-    courses:{
-      "COMP 3607":[]
-    },
-    name:"Phaedra Mohammed"
+async function getUser(email, db){
+
+  const doc = await db.collection("users").doc(email).get();
+
+  if(doc.exists){
+    return doc.data();
   }
-};
 
-function validateEmail(email){
-  return users[email] !== undefined;
-}
-
-function getUser(email){
-  if(!validateEmail(email))
-    throw `${email} not recognized!`
-  return users[email];
-}
-
-function toArray(){
-
+  return {
+    name:"Unkown",
+    courses: {}
+  };
 }
 
 async function getCourseTopics(email, course, db){
-  if(!validateEmail(email))
-    throw `${email} not recognized!`
-
-  let topics = users[email].courses[course]; 
-
-  if(!topics)
-    throw `Unkown course ${course} for ${email}`;
 
   const doc = await db.collection("users").doc(email).get();
 
@@ -60,23 +24,19 @@ async function getCourseTopics(email, course, db){
 }
 
 async function saveCourseTopics(email, course, topics, db){
-  let doc = users[email]; 
 
-  if(!doc)
-    throw `${email} not recognized!`
-  let key = doc.courses[course]; 
-  if(!key)
-    throw `Unkown course ${course} for ${email}`;
-  users[email].courses[course] = [...topics];
+  const doc = await db.collection("users").doc(email).get();
 
+  if(!doc.exists)
+    throw 'unkown email '+email;
+
+  const newDoc = doc.data();
   
-  for(let topic of topics){
-    db.collection('users').doc(email).set(doc);
-  }
+  newDoc['courses'][course] = topics;
 
-  await (data);
+  await db.collection('users').doc(email).set(newDoc);
 
 }
 
 
-export {getUser, getCourseTopics, saveCourseTopics, validateEmail};
+export {getUser, getCourseTopics, saveCourseTopics};
